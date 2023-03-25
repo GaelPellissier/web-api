7f71c2c7d6a6ee999811a8cea703df260347e8a8
GaelPellissier <gael.pellissier@gmail.com>
2023-03-25 16:48:23 +0100

    Initial commit

---
+++ src/main/java/com/wapps/homestock/webapi/HomeStockWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.wapps.homestock.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeStockWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeStockWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wapps/homestock/webapi/HomeStockWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wapps.homestock.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeStockWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
